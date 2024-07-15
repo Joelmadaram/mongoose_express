10e79c6d641fc35ea0d57f1388b91a25bc181099
madaram.joel@gmail.com <madaram.joel@gmail.com>
2024-07-15 15:05:45 -0500

    Added Full CRUD operations with Mongoose and Express.js

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
 
-
 const Product  = require('./models/product');
 
 mongoose.connect('mongodb://127.0.0.1:27017/farmStand')
@@ -21,14 +20,17 @@ app.set('view engine', 'ejs');
 app.use(express.urlencoded({ extended: true }));
 app.use(methodOverride('_method'))
 
+const categories = ['Fruit','Vegetables','Dairy'];
+
+
 app.get('/products', async(req, res) => {
     const products = await Product.find({})
     res.render('products/index', { products });
-});
+})
 
 app.get('/products/new', (req, res) => {
-    res.render('products/new');
-});
+    res.render('products/new', { categories })
+})
 
 app.post('/products',async(req, res) => {
     const newProduct = new Product(req.body);
@@ -46,14 +48,21 @@ app.get('/products/:id', async(req, res) => {
 app.get('/products/:id/edit', async(req, res) => {
     const { id } = req.params;
     const product = await Product.findById(id);
-    res.render('products/edit', { product })
-});
+    res.render('products/edit', { product, category})
+})
 
 app.put('/products/:id', async(req,res) => {
-    console.log(req.body);
-    res.send('PUT');
+    const { id } = req.params;
+    const product = await Product.findByIdAndUpdate(id, req.body, { runValidators: true, new: true });
+    res.redirect(`/products/${product._id}`);
+})
+
+app.delete('/products/:id', async(req,res)  => {
+    const { id } = req.params;
+    const deletedProduct = await Product.findByIdAndDelete(id);
+    res.redirect('/products');
 })
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
-});
+})
